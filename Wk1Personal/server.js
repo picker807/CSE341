d07365467912048bb0b44b877ff54cc6afc0d7c9
picker807 <60443210+picker807@users.noreply.github.com>
2023-10-02 19:32:24 -0400

    Added code to server.js for front end interface

--- Wk1Personal/server.js
+++ Wk1Personal/server.js
@@ -5,10 +5,21 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 
 // json middleware handling
-app.use(bodyParser.json());
+app
+	.use(bodyParser.json())
 
-//routes
-app.use("/", require("./routes"));
+	.use((req, res, next) => {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader(
+			'Access-Control-Allow-Headers',
+			'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+		);
+		res.setHeader('Content-Type', 'application/json');
+		res.setHeader('Access-Control-Allow_methods', 'GET, POST, PUT, DELETE, OPTIONS');
+		next();
+	})
+
+	.use("/", require("./routes"));
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
